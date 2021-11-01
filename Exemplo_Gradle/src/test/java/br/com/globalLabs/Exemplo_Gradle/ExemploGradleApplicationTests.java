11b140f8df6d7c01eed283c0b688e4c5fb9d4f0a
Artur R Campos <arturrc10@outlook.com>
2021-11-01 07:42:53 -0300

    remembering language features

---
+++ Exemplo_Gradle/src/test/java/br/com/globalLabs/Exemplo_Gradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globalLabs.Exemplo_Gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Exemplo_Maven/src/test/java/br/com/globalLabs/Exemplo_Maven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globalLabs.Exemplo_Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
